b6a4c8e2ca6b7a1abd3a26696b560ef94f8ed942
akyl <akylmukatay@gmail.com>
2023-09-27 16:31:46 +0600

    configure

--- Chats/Chats/LoginViewController.swift
+++ Chats/Chats/LoginViewController.swift
@@ -9,33 +9,68 @@ import UIKit
 import SnapKit
 
 class LoginViewController: UIViewController {
-    
     private let headerStack = UIStackView()
     private let profileImage = UIImageView()
     private let textFieldsStack = UIStackView()
     private let button = UIButton(type: .system)
+    private let registrationButton = UIButton(type: .system)
+    
+    private let welcomeLabel = UILabel()
+    private let nameLabel = UILabel()
+    private let descriptionLabel = UILabel()
+    private let loginTextField = UITextField()
+    private let passwordTextField = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        configure()
+        addSubviews()
+        setViewConstraints()
+        stylizeViews()
     }
     
-    func configure() {
+    func addSubviews() {
         view.addSubview(headerStack)
-        let welcomeLabel = UILabel()
-        let nameLabel = UILabel()
-        let descriptionLabel = UILabel()
         headerStack.addArrangedSubview(welcomeLabel)
         headerStack.addArrangedSubview(nameLabel)
         headerStack.addArrangedSubview(descriptionLabel)
-        headerStack.axis = .vertical
-        headerStack.spacing = 4
+        
+        view.addSubview(profileImage)
+        view.addSubview(textFieldsStack)
+        view.addSubview(registrationButton)
+        view.addSubview(button)
+    }
+    
+    func setViewConstraints() {
         headerStack.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(80)
             make.left.equalToSuperview().inset(40)
             make.width.equalTo(200)
         }
+        profileImage.snp.makeConstraints { make in
+            make.top.equalTo(headerStack.snp.bottom).offset(64)
+            make.left.equalToSuperview().inset(80)
+            make.width.height.equalTo(90)
+        }
+        textFieldsStack.snp.makeConstraints { make in
+            make.top.equalTo(profileImage.snp.bottom).offset(32)
+            make.left.equalToSuperview().inset(80)
+            make.right.equalToSuperview()
+        }
+        registrationButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(40)
+            make.centerX.equalToSuperview()
+        }
+        button.snp.makeConstraints { make in
+            make.bottom.equalTo(registrationButton.snp.bottom).inset(60)
+            make.left.right.equalToSuperview().inset(80)
+            make.height.equalTo(48)
+        }
+    }
+    
+    func stylizeViews() {
+        headerStack.axis = .vertical
+        headerStack.spacing = 4
         
         welcomeLabel.text = "Welcome to"
         welcomeLabel.font = UIFont.systemFont(ofSize: 24, weight: .medium)
@@ -47,42 +82,30 @@ class LoginViewController: UIViewController {
         descriptionLabel.numberOfLines = 0
         descriptionLabel.textColor = .systemGray
         
-        view.addSubview(profileImage)
         profileImage.image = UIImage(named: "user1")
-        profileImage.snp.makeConstraints { make in
-            make.top.equalTo(headerStack.snp.bottom).offset(64)
-            make.left.equalToSuperview().inset(80)
-            make.width.height.equalTo(90)
-        }
         profileImage.layer.cornerRadius = 16
         profileImage.clipsToBounds = true
         
-        view.addSubview(textFieldsStack)
-        let loginTextField = UITextField()
-        let passwordTextField = UITextField()
         textFieldsStack.addArrangedSubview(loginTextField)
         textFieldsStack.addArrangedSubview(passwordTextField)
         textFieldsStack.axis = .vertical
         textFieldsStack.spacing = 40
-        textFieldsStack.snp.makeConstraints { make in
-            make.top.equalTo(profileImage.snp.bottom).offset(32)
-            make.left.equalToSuperview().inset(80)
-            make.right.equalToSuperview()
-        }
+        
         loginTextField.placeholder = "Login"
         loginTextField.font = UIFont.systemFont(ofSize: 18)
         addBottomBorder(to: loginTextField)
+        
         passwordTextField.placeholder = "Password"
         passwordTextField.isSecureTextEntry = true
         passwordTextField.font = UIFont.systemFont(ofSize: 18)
         addBottomBorder(to: passwordTextField)
         
-        view.addSubview(button)
-        button.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().inset(80)
-            make.left.right.equalToSuperview().inset(96)
-            make.height.equalTo(48)
-        }
+        registrationButton.setTitle("Don't have account? Sing up", for: .normal)
+        registrationButton.backgroundColor = .clear
+        registrationButton.addTarget(
+            self, action: #selector(registrationButtonTapped), for: .touchUpInside
+        )
+        
         button.backgroundColor = UIColor.systemGreen
         button.setTitle("Connect", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
@@ -107,6 +130,8 @@ class LoginViewController: UIViewController {
         print("buttonTapped()")
     }
 
-
+    @objc func registrationButtonTapped() {
+        print("registrationButtonTapped()")
+    }
 }
 
